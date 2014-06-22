33d51aae5f98bef3a732d622593c7ab38a141c10
Artem Abashev <artem@abashev.com>
2014-06-22 21:11:33 +0400

    Added cryptography types.

---
+++ Serialization/IEncrypter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Touch.Serialization
+{
+    /// <summary>
+    /// Data encrypter.
+    /// </summary>
+    public interface IEncryper
+    {
+        /// <summary>
+        /// Encrypt data.
+        /// </summary>
+        byte[] Crypt(byte[] data);
+
+        /// <summary>
+        /// Encrypt data with salt.
+        /// </summary>
+        byte[] Crypt(byte[] data, byte[] salt);
+    }
+}
---
+++ Serialization/IHashGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Touch.Serialization
+{
+    /// <summary>
+    /// Data hash generator.
+    /// </summary>
+    public interface IHashGenerator
+    {
+        /// <summary>
+        /// Generate hash for data.
+        /// </summary>
+        string Generate(byte[] data);
+    }
+}
