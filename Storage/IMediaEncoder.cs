bc97f01cc4b194bdc3c4b9d1e6fb0fa07ca63ec9
Artem Abashev <artem@abashev.com>
2014-07-10 21:23:49 +0400

    Media encoder types.

---
+++ Storage/IMediaEncoder.cs
@@ -0,0 +1,13 @@
+﻿namespace Touch.Storage
+{
+    /// <summary>
+    /// Media encoder.
+    /// </summary>
+    public interface IMediaEncoder
+    {
+        /// <summary>
+        /// Encoded the provided storage file.
+        /// </summary>
+        MediaEncoderJob Encode(string source, string output);
+    }
+}
---
+++ Storage/MediaEncoderJob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Touch.Storage
+{
+    /// <summary>
+    /// Media encoder job.
+    /// </summary>
+    public abstract class MediaEncoderJob
+    {
+        public virtual Guid Token { get; protected set; }
+
+        public virtual string Source { get; protected set; }
+
+        public virtual string Output { get; protected set; }
+
+        public virtual DateTime Started { get; protected set; }
+
+        public virtual MediaEncoderJobStatus Status { get; protected set; }
+    }
+
+    public enum MediaEncoderJobStatus
+    {
+        Started,
+        Complete,
+        Failed
+    }
+}
