73aa2c2f53a82cfd88c4e0fdcc9503b89fccf046
Artem Abashev <artem@abashev.com>
2014-07-24 17:55:01 +0400

    Added dependencies provider contract.

---
+++ Providers/IDependenciesProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace Touch.Providers
+{
+    /// <summary>
+    /// Dependencies provider.
+    /// </summary>
+    public interface IDependenciesProvider
+    {
+        /// <summary>
+        /// Resolve a dependency.
+        /// </summary>
+        T Resolve<T>() where T : class;
+    }
+}
